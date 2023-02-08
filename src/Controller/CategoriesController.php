b5cd06ea9753f51049152281556b27cfd38e29d2
Fabien-Scetbon <fscetbon@gmail.com>
2023-02-08 11:48:04 +0100

    controllers

---
+++ src/Controller/CategoriesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categories;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/categories', name: 'categories_')]
+class CategoriesController extends AbstractController
+{
+    #[Route('/{slug}', name: 'list')]
+    public function details(Categories $category): Response
+    {
+        return $this->render(
+            'categories/list.html.twig',
+            compact('category')
+        );
+    }
+}
