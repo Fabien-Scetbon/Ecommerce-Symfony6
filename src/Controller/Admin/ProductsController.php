7322cdc4aaa7c304ec53ca6d6ca947b4c57f3b43
Fabien-Scetbon <fscetbon@gmail.com>
2023-02-14 11:29:27 +0100

    add product form

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -3,12 +3,17 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Products;
+use App\Form\ProductsFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
-#[Route('/admin/produits', name :'admin_products_')]
-class ProductsController extends AbstractController {
+#[Route('/admin/produits', name: 'admin_products_')]
+class ProductsController extends AbstractController
+{
 
     #[Route('/', name: 'index')]
     public function index(): Response
@@ -17,9 +22,21 @@ class ProductsController extends AbstractController {
     }
 
     #[Route('/ajout', name: 'add')]
-    public function add(): Response
+    public function add(Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
     {
-        return $this->render('admin/products/index.html.twig');
+        // on cree un nouveau produit
+        $product = new Products;
+
+        // on cree un formulaire
+        $productForm = $this->createForm(ProductsFormType::class, $product);
+
+        return $this->render('admin/products/add.html.twig', [
+            'productForm' => $productForm->createView()
+        ]);
+
+        // on traite la requete du form
+
+
     }
 
     #[Route('/edition/{id}', name: 'edit')]
